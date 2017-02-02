e7b43223e950cf111414bc2d1d71f92b900fa27a
Max Peiros <mpeiros@gmail.com>
2017-02-02 13:17:45 -0800

    Format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,13 @@
+var moment = require('moment');
+
+// Jan 1st 1970 00:00:00 am
+
+// var date = new Date();
+// var months = ['Jan, 'Feb'];
+// console.log(date.getMonth());
+
+// var date = moment();
+// console.log(date.format('MMM Do, YYYY'));
+
+var date = moment();
+console.log(date.format('h:mm a'));
